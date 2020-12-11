957da9d3073eda0ea50e84270413794629a40048
Kaloyan Kulov <kaloyankulov2003kk@gmail.com>
2020-12-11 19:41:38 +0200

    Updated tests

--- Assignments/Exam Preparation/August_16_2020/tests/test_hardware.py
+++ Assignments/Exam Preparation/August_16_2020/tests/test_hardware.py
@@ -1,51 +1,38 @@
 import unittest
 
 from project.hardware.hardware import Hardware
-from project.hardware.power_hardware import PowerHardware
 from project.software.software import Software
 
 
 class TestHardware(unittest.TestCase):
     def setUp(self):
-        self.hardware = Hardware('CPU', 'Hardware', 100, 100)
-        self.power_hardware = PowerHardware('Test', 100, 100)
+        self.hardware = Hardware('Test', 'Power', 100, 100)
 
-    def test_initialization(self):
-        self.assertEqual(self.hardware.name, 'CPU')
-        self.assertEqual(self.hardware.type, 'Hardware')
+    def test_correct_initialization(self):
+        self.assertEqual(self.hardware.name, 'Test')
+        self.assertEqual(self.hardware.type, 'Power')
         self.assertEqual(self.hardware.capacity, 100)
         self.assertEqual(self.hardware.memory, 100)
+        self.assertEqual(self.hardware.software_components, [])
 
-    def test_install_should_raise_error_when_there_is_not_enough_memory_or_capacity(self):
-        software = Software('Windows', 'Software', 110, 110)
+    def test_install_should_add_software_to_the_software_components(self):
+        software = Software('Windows', 'Light', 50, 50)
+        self.hardware.install(software)
+        self.assertEqual(len(self.hardware.software_components), 1)
+        # self.assertEqual(len(self.hardware.software_components), 1)
+
+    def test_install_should_raise_error_if_capacity_or_memory_are_more_than_the_hardware_ones(self):
+        software = Software('Windows', 'OS', 150, 150)
 
         with self.assertRaises(Exception) as cm:
             self.hardware.install(software)
         self.assertEqual(str(cm.exception), "Software cannot be installed")
 
-    def test_install_should_add_a_software_to_the_software_components(self):
-        software = Software('Windows', 'Software', 100, 100)
-
-        self.assertEqual(len(self.hardware.software_components), 0)
-        self.hardware.install(software)
-        self.assertEqual(len(self.hardware.software_components), 1)
-
     def test_uninstall_should_remove_software_from_the_software_components(self):
-        software = Software('Windows', 'Software', 100, 100)
+        software = Software('Windows', 'OS', 25, 25)
         self.hardware.install(software)
-
-        self.assertEqual(len(self.hardware.software_components), 1)
         self.hardware.uninstall(software)
         self.assertEqual(len(self.hardware.software_components), 0)
-
-    def test_uninstall_should_not_remove_the_software_if_it_is_not_in_the_software_components(self):
-        software = Software('Windows', 'Software', 100, 100)
-        software2 = Software('MacOS', 'Software', 100, 100)
-        self.hardware.install(software)
-
-        self.assertEqual(len(self.hardware.software_components), 1)
-        self.hardware.uninstall(software2)
-        self.assertEqual(len(self.hardware.software_components), 1)
 
 
 if __name__ == '__main__':
